5fdcb640c9d88616bdda9d7aa665306deef710ec
nanagyanfosu <nanagyanfosu@gmail.com>
2025-08-07 21:40:03 +0000

    Abstract Classes and Methods task completed

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle(double radius) : Shape
+{
+    private double radius = radius;
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;    
+    }
+}
+
+class Rectangle(double length, double breadth) : Shape
+{
+    public double length = length, breadth = breadth;
+
+    public override double GetArea() {
+        return length * breadth;
+    }
+}
+    class Program
+    {
+        static void Main()
+        {
+            Shape circle = new Circle(10);
+            Console.WriteLine("Area of circle: " + circle.GetArea());
+            Shape rectangle = new Rectangle(10, 20);
+            Console.WriteLine("Area of rectangle: " + rectangle.GetArea());
+        }
+
+
+    }
+
+
+
+
