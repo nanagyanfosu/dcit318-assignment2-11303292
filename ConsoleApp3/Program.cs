a153b5e8cbec46b706390b1edf56d0e2c06d9378
nanagyanfosu <nanagyanfosu@gmail.com>
2025-08-07 22:05:03 +0000

    Interfaces task completed

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(IMovable car, IMovable bicycle)
+    {     
+        car.Move();
+
+        bicycle.Move();
+    }
+}
