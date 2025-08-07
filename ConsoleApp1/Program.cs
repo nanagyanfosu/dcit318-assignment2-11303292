c247e0157895a09b99fc9433eccc93b951a91228
nanagyanfosu <nanagyanfosu@gmail.com>
2025-08-07 20:46:44 +0000

    Inheritance and Method Overriding task complete

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Animal() {
+    public virtual void MakeSound() {
+        Console.WriteLine("Some generic sound");
+    }
+
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Animal animal = new();
+        animal.MakeSound();
+        Dog dog = new();
+        dog.MakeSound();
+        Cat cat = new();
+        cat.MakeSound();
+    }
+}
+
+
